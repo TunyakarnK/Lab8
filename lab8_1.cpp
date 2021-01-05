658a37bcbf738450236f12fcc7e0be419292aa09
TunyakarnK <tunyakarn2544@gmail.com>
2021-01-05 16:21:52 +0700

    Done

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 int main(){	
+int y=0;
+	double PB,I,ir,T,P,NB;
 	cout << "Enter initial loan: ";
+	cin >> PB;
 	cout << "Enter interest rate per year (%): ";
+	cin >> ir;
 	cout << "Enter amount you can pay per year: ";
+	cin >> P;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +19,28 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+
+	do{
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << y+1; 
+	if(y>0){
+		PB=NB;
+	}else{
+		PB=PB;
+	}
+	cout << setw(13) << left << PB;
+	I= PB*(ir/100);
+	cout << setw(13) << left << I;
+	T=PB+I;
+	cout << setw(13) << left << T;
+	if(T<P){
+		P=T;
+	}
+	cout << setw(13) << left << P;
+	NB =T-P;
+	cout << setw(13) << left << NB;
+	y++;
 	cout << "\n";	
-	
+	}while (NB != 0);
 	return 0;
 }
